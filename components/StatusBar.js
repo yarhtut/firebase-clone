9729026264256e42d0bb5a6fc12d830b1e78eaa7
Yar Thet Pan Kyaw Htut <yar@boost.co.nz>
2016-05-19 08:51:04 +1200

    reinitilized files

---
+++ components/StatusBar.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import  styles from '../styles.js';
+
+
+class StatusBar extends Component {
+  render() {
+    return (
+      <View>
+        <View style={styles.statusbar}/>
+        <View style={styles.navbar}>
+          <Text style={styles.navbarTitle}>{this.props.title}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+module.exports = StatusBar;
